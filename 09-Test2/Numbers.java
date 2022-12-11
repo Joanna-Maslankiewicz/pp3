a4a5c3f5f7cac33078d1e2bfb20992e717e0a495
Joanna-Maslankiewicz <s222964@student.uek.krakow.pl>
2022-12-11 14:10:00 +0100

    zadanie 1

---
+++ 09-Test2/Numbers.java
@@ -0,0 +1,37 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Numbers {
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+
+    public Numbers(int a, int b, int c, int d, int e) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+    }
+
+    public boolean different() {
+        int[] values = { a, b, c, d, e };
+        Set<Integer> foundNumbers = new HashSet<Integer>();
+
+        for (int value : values) {
+            if (foundNumbers.contains(value)) {
+                return false;
+            }
+            foundNumbers.add(value);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Numbers n1 = new Numbers(9, 7, 4, 3, 7);
+        System.out.println(n1.different());
+    }
+
+}
