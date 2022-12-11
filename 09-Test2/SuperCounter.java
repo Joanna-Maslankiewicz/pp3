ff7adcaf8e29ce8b4059b673cf494a574d3270c3
Joanna-Maslankiewicz <s222964@student.uek.krakow.pl>
2022-12-11 15:39:10 +0100

    zadanie 5

---
+++ 09-Test2/SuperCounter.java
@@ -0,0 +1,23 @@
+public class SuperCounter extends Counter {
+    public Counter counter;
+
+    public SuperCounter(int counter) {
+        super(counter);
+    }
+
+    public void addN(int n) {
+        int i = 0;
+        while (i < n) {
+            add1();
+            i++;
+        }
+    }
+
+    // sprawdzenie
+    public static void main(String[] args) {
+        SuperCounter s1 = new SuperCounter(123);
+        s1.addN(100);
+        System.out.println(s1.getCounter());
+    }
+
+}
