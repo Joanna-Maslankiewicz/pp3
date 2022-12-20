886c0ad44a6be7e91d4a1c7faab35f4334a5909d
Joanna-Maslankiewicz <s222964@student.uek.krakow.pl>
2022-12-20 12:55:14 +0100

    test

--- 10-AbstractClasses/Main_8_9_10.java
+++ 10-AbstractClasses/Main_8_9_10.java
@@ -2,8 +2,10 @@ public class Main_8_9_10 {
     public static void main(String[] args) {
         SMS s1 = new SMS("hej", 123456789);
         s1.send();
+        System.out.println("length: " + s1.charNumber());
 
         Email e1 = new Email("hej", "przywitanie", "skhabvksudbfvk");
         e1.send();
+        System.out.println("length: " + e1.charNumber());
     }
 }
